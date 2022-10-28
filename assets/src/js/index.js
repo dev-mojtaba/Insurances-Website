619bfc667df4ec3dc15caafa4d386e78fcf50631
dev-mojtaba <mojtaba.dev.5858@gmail.com>
2022-10-28 23:53:06 +0330

    all js file in src/js folder minified

---
+++ assets/src/js/index.js
@@ -0,0 +1,7 @@
+import './scripts/whenPageLoaded';
+import './scripts/navbarClickableMenu';
+import './scripts/carInsurance';
+import './scripts/financeInsurance';
+import './scripts/healthInsurance';
+import './scripts/houseInsurance';
+import './scripts/swiper.config';
